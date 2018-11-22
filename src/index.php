da8056e61a93ed3892637daafa29eee35c794401
robert <oranje@mail.com>
2018-11-22 10:21:09 +0100

    update

--- src/index.php
+++ src/index.php
@@ -20,4 +20,4 @@ $source = "../images/" . $filename;
 $destination = "../images/compressed/" . $filename;
 $compress = (new Image())->compress($source, $destination); // go with defaults for quality(80) and maxWidth(800px)
 
-echo 'copy back to Github repo';
+echo 'and now update the file';
